66c023fabe2c7c3e32a563ce7b3edbcc18617c58
MrFogg866 <philiplynch866@gmail.com>
2022-05-16 13:46:47 +0000

    add validation to check 7 values are entered or not

--- run.py
+++ run.py
@@ -14,13 +14,29 @@ SHEET = GSPREAD_CLIENT.open('project-3')
 
 def get_scoops_data():
     """
-    Get Scoops sold data sold each day from user
+    Get scoops sold data sold each day from user
     """
     print("Please enter how many scoops sold today.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    print("Data should be seven numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60,70\n")
 
     data_str = input("Enter your data here: ")  
-    print(f"The data provided is {data_str}")
+    scoops_data = data_str.split(",")
+    validate_data(scoops_data)
+
+def validate_data(values):  
+    """
+    inside The try, converts all string values into integers
+    raises ValueError if strings cannot be converted into integers,
+    or if there arnt exactly 7 values.
+    """ 
+
+    try:
+        if len(values) != 7:
+            raise ValueError(
+                f"Ooops, please enter data for all 7 items, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_scoops_data()
