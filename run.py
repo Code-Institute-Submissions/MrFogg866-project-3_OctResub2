27697005ad785f3b8fec3606fe54fb6f55238e39
MrFogg866 <philiplynch866@gmail.com>
2022-05-17 12:03:28 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,7 +22,7 @@ def get_scoops_data():
         print("Data should be seven numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60,70\n")
 
-        data_str = input("Enter your data here: ")  
+        data_str = input("Enter How many scoops sold today here:\n")  
 
         scoops_data = data_str.split(",")
         
